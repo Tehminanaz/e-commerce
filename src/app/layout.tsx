86e6fb9ecc869d0798a29b67f3a0a30ff4433a04
Tehminanaz <tn0457584@gmail.com>
2024-12-18 01:01:29 +0500

    Updated responsive styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import TopNav from "./components/TopNav";
-import Footer from "./components/Footer";
+
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,7 +21,7 @@ export default function RootLayout({
       <body className={inter.className}>
        
         {children}
-        <Footer/>
+        
         </body>
     </html>
   );
